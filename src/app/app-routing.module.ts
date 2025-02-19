354fadbc406a6e32ecd8132ce2eccb455317eac4
WalaBenRhoumaa <walaabenrhouma@gmail.com>
2025-02-20 00:40:09 +0100

    workshop2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home.component/home.component';
+import { ResidencessComponentComponent } from './residences/residencess-component/residencess-component.component';
+import { ResidenceDetailsComponentComponent } from './residences/residence-details-component/residence-details-component';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { AddProductComponent } from './residences/add-residence-component/add-residence-component.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponentComponent },
+  { path: 'residences', component: ResidencessComponentComponent },
+  { path: 'residences/:id', component: ResidenceDetailsComponentComponent },
+  { path: 'add-residence', component: AddProductComponent }, // Fixed the routing
+  { path: '**', component: NotFoundComponentComponent }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
